63391b6d2994c793c3461e98ef0ad975ca897b3e
apxxxxxxe <calcium629@gmail.com>
2022-06-26 23:40:29 +0900

    update: Google Analyticsを有効化

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
+import Script from "next/script";
 import "styles/blog.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-X8CTK9HSVL"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-X8CTK9HSVL');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
