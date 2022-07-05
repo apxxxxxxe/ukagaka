559fc5038cbf28d82d47fbefe5bada34db982178
apxxxxxxe <calcium629@gmail.com>
2022-07-05 14:33:09 +0900

    remove: メニューからblogボタンを削除

--- utils/Layout.tsx
+++ utils/Layout.tsx
@@ -39,11 +39,6 @@ export default function Layout({ children, title = "" }): JSX.Element {
               <a>tips</a>
             </Link>
           </p>
-          <p>
-            <Link href="/blog/">
-              <a>blog</a>
-            </Link>
-          </p>
         </div>
         <div id="container" className="inner">
           {children}
