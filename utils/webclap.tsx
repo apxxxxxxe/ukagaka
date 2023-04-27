19e2124df7f68d45f7995f99c7cbe003a963fe9a
apxxxxxxe <calcium629@gmail.com>
2023-04-27 20:36:38 +0900

    fix: webclap

--- utils/webclap.tsx
+++ utils/webclap.tsx
@@ -1,27 +1,24 @@
 const WebClapBox = () => {
-	const opener = () => {
-		window.open(
-			"http://clap.webclap.com/clap.php?id=apxxxxxxe",
-			"webclap",
-			"toolbar=no,location=no,directories=no,status=no,scrollbars=yes,resizable=yes"
-		)
-		return false
-	}
+  const opener = () => {
+    window.open(
+      "http://clap.webclap.com/clap.php?id=apxxxxxxe",
+      "webclap",
+      "toolbar=no,location=no,directories=no,status=no,scrollbars=yes,resizable=yes"
+    );
+    return false;
+  };
 
-	return (
-		<div className="webclap-box">
-			<a
-				href="http://clap.webclap.com/clap.php?id=apxxxxxxe"
-				target="_blank"
-				onClick={opener}
-			>
-				<img
-					src={`http://img.webclap.com/webclap/button/off.php?img=39`}
-					style={{ width: "auto" }}
-				/>
-			</a>
-		</div>
-	)
-}
+  return (
+    <div className="webclap-box">
+      <a
+        href="http://clap.webclap.com/clap.php?id=apxxxxxxe"
+        target="_blank"
+        onClick={opener}
+      >
+        WEB拍手
+      </a>
+    </div>
+  );
+};
 
-export default WebClapBox
+export default WebClapBox;
