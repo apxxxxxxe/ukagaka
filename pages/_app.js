8b2ea8494d21238fb57508e08c1a106dbb1ac224
apxxxxxxe <calcium629@gmail.com>
2024-02-07 23:02:25 +0900

    remove google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,9 @@
-import Script from "next/script"
 // import "styles/main.scss"
 import "styles/tailwind.css"
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
-			<Script
-				src="https://www.googletagmanager.com/gtag/js?id=G-X8CTK9HSVL"
-				strategy="afterInteractive"
-			/>
-			<Script id="google-analytics" strategy="afterInteractive">
-				{`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-X8CTK9HSVL');
-        `}
-			</Script>
 			<Component {...pageProps} />
 		</>
 	)
