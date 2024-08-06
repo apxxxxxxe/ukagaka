e00f9c70e280420232b562e1cefcfd1a2c756569
apxxxxxxe <calcium629@gmail.com>
2024-08-06 22:12:32 +0900

    💄faviconを追加

--- utils/Layout.tsx
+++ utils/Layout.tsx
@@ -50,7 +50,7 @@ export default function Layout({
 				<title>{pageTitle}</title>
 				<link
 					rel="shortcut icon"
-					href="favicon.ico"
+					href="/favicon.ico"
 					type="image/vnd.microsoft.icon"
 				/>
 			</Head>
